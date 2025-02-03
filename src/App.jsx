09f29de396f31c693b5fbbcd608bbed36a51dbbd
Roamer15 <ianseeksbeta@gmail.com>
2025-02-03 16:47:42 +0100

    Other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Counter } from './features/counter/Counter'
+import logo from './logo.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo" />
+        <p>Edit <code>src/App.jsx</code> and save to reload</p>
+        <Counter />
+        <span>
+          <span>Learn </span>
+          <a 
+            className='App-link'
+            href="https://reactjs.org"
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            React
+          </a>
+          <span> and </span>
+          <a
+           className='App-link'
+           href="https://redux.js.org/tutorials/essentials/part-2-app-structure"
+           target='_blank'
+           rel='noopener noreferrer'
+           >
+            Redux
+           </a>
+        </span>
+      </header>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './index.css'
+
+import App from './App.jsx'
+import { store } from './app/store.js'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+     <React.Strictmode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+     </React.Strictmode>
 )
